83f54e6c874ff70afd5df773ba99352f96228628
johan974 <jj974@upcmail.nl>
2022-11-04 21:30:23 +0100

    Added: dynamic queues, just code, no tests.

---
+++ README_johan.md
@@ -0,0 +1,23 @@
+# Steps to start the project
+- You downloaded this project. 
+
+# Creating a startup icon for Docker MQ
+- Copy a short-link on your desktop. 
+  - Tab: snelkoppeling
+  - Path: K:\A__IntelliJ_projects\messaging-microservices-rabbitmq\docker\DockerRabbitMq.bat
+  - Execute in:  K:\A__IntelliJ_projects\messaging-microservices-rabbitmq\docker
+- All startup stuff is in the 'docker' subfolder
+- Double click on the desktop quick link. After a while the RabbitMQ is running 
+as a Docker container. 
+
+# Startup the demo
+- Set JDK via 'project settings' on JDK 11. 
+- Start the applications
+  - Consumer: Maven clean install ... and start the Application (right click)
+  - Producer: Maven clean install ... and start the Application (right click)
+- Start the console: browse to localhost:15672 and login with guest/guest
+- Send a message via Postman: 
+  - Url: http://localhost:8081/api/produce
+  - Type: POST
+  - Body: raw > json > { "userId":"ID1", "userName":"johan" }
+- Je ziet in RabbitMQ console dat er een bericht gestuurd is
--- microservices-messaging-producer/src/main/java/com/github/pedroluiznogueira/microservices/messaging/config/RabbitMQConfig.java
+++ microservices-messaging-producer/src/main/java/com/github/pedroluiznogueira/microservices/messaging/config/RabbitMQConfig.java
@@ -1,16 +1,24 @@
 package com.github.pedroluiznogueira.microservices.messaging.config;
 
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistry;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
 
 @Configuration
-public class RabbitMQConfig {
+public class RabbitMQConfig implements RabbitListenerConfigurer {
 
     @Value("${spring.rabbitmq.host}")
     String host;
@@ -30,14 +38,52 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+        return rabbitTemplate;
+    }
+
+    // Johan --- extra config beans
+    // Johan: added parameter: connectionFactory ... so I can use it in RabbitAdmin
+    @Bean
+    public RabbitAdmin rabbitAdmin( ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
     }
 
     @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(jsonMessageConverter());
-        return rabbitTemplate;
+    public RabbitListenerEndpointRegistry rabbitListenerEndpointRegistry(){
+        return new RabbitListenerEndpointRegistry();
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+    @Bean
+    public MappingJackson2MessageConverter consumerJackson2MessageConverter() {
+        return new MappingJackson2MessageConverter();
+    }
+
+    @Bean
+    public DefaultMessageHandlerMethodFactory messageHandlerMethodFactory() {
+        DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
+        factory.setMessageConverter(consumerJackson2MessageConverter());
+        return factory;
+    }
+//    @Bean
+//    public MessageConverter jsonMessageConverter() {
+//        return new Jackson2JsonMessageConverter();
+//    }
+    @Override
+    public void configureRabbitListeners(final RabbitListenerEndpointRegistrar registrar) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setPrefetchCount(1);
+        factory.setConsecutiveActiveTrigger(1);
+        factory.setConsecutiveIdleTrigger(1);
+        factory.setConnectionFactory(connectionFactory());
+        registrar.setContainerFactory(factory);
+        registrar.setEndpointRegistry(rabbitListenerEndpointRegistry());
+        registrar.setMessageHandlerMethodFactory(messageHandlerMethodFactory());
     }
 }
